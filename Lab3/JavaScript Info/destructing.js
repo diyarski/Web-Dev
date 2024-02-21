ceaa9d96e32a6b533255782811517549aefcee47
diyarski <122877469+diyarski@users.noreply.github.com>
2024-02-21 20:51:45 +0600

    Create destructing.js

---
+++ Lab3/JavaScript Info/destructing.js
@@ -0,0 +1,10 @@
+let user = {
+    name: "John",
+    years: 30
+  };
+  
+  let {namee, years: age, isAdmin = false} = user;
+  
+  alert( namee ); // John
+  alert( age ); // 30
+  alert( isAdmin ); // false
